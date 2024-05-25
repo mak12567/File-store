0640132e9f8a347b7aad2e14594d179db546ad04
mak12567 <148629684+mak12567@users.noreply.github.com>
2024-05-25 11:43:50 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -5,4 +5,4 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("VysakhTG")
+    return web.json_response("search on telegram @ps_updates")
